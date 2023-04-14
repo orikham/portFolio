38e3c32fe06530222b72cd316ef6ab59c5ddebc4
orikham <orikham@hotmail.fr>
2023-04-14 15:02:46 +0200

    portfolio 1er commit

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>LaBonneBlog</title>
+    <link rel="stylesheet" href="./CSS/style.css">
+</head>
+<body>
+    <header class="mainHeaderRes1 mainHeaderRes2 mainHeaderRes3" >
+        <div class="row-limi-size">
+            <div id="logo"><a href="./index.php"> <img src="./img/logoDef.png"> </a></div>
+            <nav>
+                <ul>
+                    <li><a href="./CVDimitri.php">Présentation</a></li>
+                    <li><a href="./présentation.php">Formation</a></li>
+                    <li><a href="./présentation.php">Mes Projets</a></li>
+                    <li><a href="./présentation.php">Me Contacter</a></li>
+                </ul>
+            </nav>
+            
+        </div>    
+    </header>
+
+    <section>
+            
+            <div class="articles row-limi-size">
+            <h1>bienvenue</h1>
+            <aside>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores voluptatum nulla fugit, tempore quos, eos libero similique unde at molestiae cupiditate commodi. Minima impedit dolor rem nemo expedita error consectetur.lorem Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit vitae perspiciatis impedit magni incidunt, fuga eius, ducimus numquam beatae excepturi, illo nam ab similique consequuntur voluptas rem illum perferendis assumenda. lorem
+                    </p>
+            </aside>
+            </div>
+    </section>
